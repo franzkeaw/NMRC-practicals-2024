d9366632cad5590ae33b39e278530603bb2e62c0
Pietro Marchesi <pietromarchesi92@gmail.com>
2018-11-27 12:07:59 +0100

    Added utils, environment, and getting_started practical

---
+++ utils_assignment_1.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['figure.dpi']= 80
+
+def plot_single_neuron_output(w, p, b, transfer_function):
+    output = transfer_function(w * p - b)
+
+    lim = 4
+    xx  = np.arange(-lim,lim,0.01)
+    yy  = transfer_function(w * xx - b)
+    f, ax = plt.subplots()
+    ax.set_xlim(-lim, lim)
+    ax.set_xlabel('Input')
+    ax.set_ylabel('Output')
+    ax.plot(xx,yy)
+    ax.set_title('Function: %s; w=%s, p=%s, b=%s'
+                 % (transfer_function.__name__, w, p, b))
+
+    ax.axvline(p, c='r', linestyle=':')
+    ax.axhline(output, c='r', linestyle=':')
+    ax.scatter(p, output, c='r', s=50)
+    ax.set_ylim([-1.3, 1.3])
+    plt.show()
