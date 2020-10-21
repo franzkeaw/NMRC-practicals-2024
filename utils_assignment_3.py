5c3ffb547c411c5df92ecafdbac8cea56d5cebf3
truikes <t.r.ruikes@uva.nl>
2020-10-21 17:14:02 +0200

    Removed excess variables and imports Removed mpl_simulation_v2 (not used in repo) Made plot_data_boundary_accuracy of mpl_simulation class, this allows to have mpl_simulation the attributes 'f' and 'ax'. With those figures can be updated in place. Previously the class would generate a new figure with each training iteration, creating the wraning 'too many plots' or something like that from matplotlib.

--- utils_assignment_3.py
+++ utils_assignment_3.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import axes3d
 import seaborn as sns
 import ipywidgets as widgets
 from IPython import display
@@ -11,7 +10,7 @@ def hardlim(x):
     return np.asarray(x > 0).astype(int)
 
 
-class Neuron():
+class Neuron:
     def __init__(self, inputs, targets, bias=None, weights=None, learning_rate=1):
 
         self.inputs = inputs
@@ -32,6 +31,7 @@ class Neuron():
             self.bias = bias
         self.weight_updates, self.bias_updates = [], []
         self.mse, self.accuracy = [], []
+        self.accuracy.append(self.evaluate_accuracy(self.inputs, self.targets))
 
     def output(self, input):
         x = np.sum(input * self.weights) + self.bias
@@ -61,58 +61,26 @@ class Neuron():
         self.accuracy.append(acc)
 
 
-def plot_data_boundary_accuracy(neuron, X, y, j=None):
-    weights = neuron.weights
-    bias = neuron.bias
-    plot_line = lambda x: (-bias - weights[0] * x) / (weights[1] + 10e-5)
-
+class mlp_simulation:
     f, ax = plt.subplots(1, 2, figsize=[15, 5])
-    ax[0].scatter(X[y == 0, 0], X[y == 0, 1], color='g', s=40)
-    ax[0].scatter(X[y == 1, 0], X[y == 1, 1], color='r', s=40)
-    
-    if j is not None:
-            ax[0].scatter(X[j, 0], X[j, 1], facecolor=sns.xkcd_rgb['bright yellow'], 
-                          edgecolor='k', s=100, lw=2)
-    
-    x1, x2 = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
-    y1, y2 = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
-    xx = np.arange(x1, x2, step=0.001)
-
-    ax[0].plot(xx, plot_line(xx), c='k')
-    ax[0].set_ylim([y1, y2])
-    ax[0].set_xlim([x1, x2])
-
-    epochs = [j + 1 for j in range(len(neuron.accuracy))]
-    ax[1].scatter(epochs, neuron.accuracy, c='k')
-    ax[1].plot(epochs, neuron.accuracy, c='k')
-    ax[1].set_ylim([50, 100])
-    ax[1].set_yticks(range(int(10 * (np.min(neuron.accuracy) // 10)), 110, 10))
-    ax[1].set_xticks(epochs)
-
-    ax[0].set_title('Decision boundary')
-    ax[1].set_title('Training accuracy')
-
-    display.display(f)
-    display.clear_output(wait=True)
-
-
-
-class mlp_simulation():
+    display_handle = display.display(f, display_id='fig')
 
     def __init__(self, X, y, learning_rate=1):
         self.X = X
         self.y = y
+        self.init_X = X  # in case init button is pressed 2nd time
+        self.init_y = y
         self.learning_rate = learning_rate
-
-    def initialize_button(self, b):
         self.neuron = Neuron(self.X, self.y, learning_rate=self.learning_rate)
-        self.neuron.accuracy.append(self.neuron.evaluate_accuracy(self.X, self.y))
-        plot_data_boundary_accuracy(self.neuron, self.X, self.y)
 
-    def train_button(self, b):
+    def initialize_button(self, _):
+        self.neuron = Neuron(self.init_X, self.init_y, learning_rate=self.learning_rate)
+        self.plot_data_boundary_accuracy()
+
+    def train_button(self, _):
         for j in range(self.neuron.n_samples):
             self.neuron.train_step(j)
-            plot_data_boundary_accuracy(self.neuron, self.X, self.y, j=j)
+            self.plot_data_boundary_accuracy(j=j)
 
     def start(self):
         button = widgets.Button(description="Initialize")
@@ -122,24 +90,39 @@ class mlp_simulation():
         button = widgets.Button(description="Train")
         display.display(button)
         button.on_click(self.train_button)
-        
-        
-class mlp_simulation_v2():
 
-    def __init__(self, X, y):
-        self.X = X
-        self.y = y
+    def plot_data_boundary_accuracy(self, j=None):
+        weights = self.neuron.weights
+        bias = self.neuron.bias
+        plot_line = lambda x: (-bias - weights[0] * x) / (weights[1] + 10e-5)
 
-    def initialize_and_train_button(self, b):
-        self.neuron = Neuron(self.X, self.y)
-        self.neuron.accuracy.append(self.neuron.evaluate_accuracy(self.X, self.y))
-        plot_data_boundary_accuracy(self.neuron, self.X, self.y)
-        
-        for j in range(self.neuron.n_samples):
-            self.neuron.train_step(j)
-            plot_data_boundary_accuracy(self.neuron, self.X, self.y, j=j)
+        self.ax[0].clear()
+        self.ax[1].clear()
 
-    def start(self):
-        button = widgets.Button(description="Initialize and train")
-        display.display(button)
-        button.on_click(self.initialize_and_train_button)
+        self.ax[0].scatter(self.X[self.y == 0, 0], self.X[self.y == 0, 1], color='g', s=40)
+        self.ax[0].scatter(self.X[self.y == 1, 0], self.X[self.y == 1, 1], color='r', s=40)
+
+        if j is not None:
+            self.ax[0].scatter(self.X[j, 0], self.X[j, 1],
+                               facecolor=sns.xkcd_rgb['bright yellow'],
+                               edgecolor='k', s=100, lw=2)
+
+        x1, x2 = self.X[:, 0].min() - 0.1, self.X[:, 0].max() + 0.1
+        y1, y2 = self.X[:, 1].min() - 0.1, self.X[:, 1].max() + 0.1
+        xx = np.arange(x1, x2, step=0.001)
+
+        self.ax[0].plot(xx, plot_line(xx), c='k')
+        self.ax[0].set_ylim([y1, y2])
+        self.ax[0].set_xlim([x1, x2])
+
+        epochs = [j + 1 for j in range(len(self.neuron.accuracy))]
+        self.ax[1].scatter(epochs, self.neuron.accuracy, c='k')
+        self.ax[1].plot(epochs, self.neuron.accuracy, c='k')
+        self.ax[1].set_ylim([50, 100])
+        self.ax[1].set_yticks(range(int(10 * (np.min(self.neuron.accuracy) // 10)), 110, 10))
+        self.ax[1].set_xticks(epochs)
+
+        self.ax[0].set_title('Decision boundary')
+        self.ax[1].set_title('Training accuracy')
+
+        self.display_handle.update(self.f)
