a40111ce16e4ce62eb22984eee35c81554037e54
Andreas Franzke <andy.franzke@gmail.com>
2024-10-17 16:59:54 +0200

    test for different seed in initialization

--- utils_assignment_3.py
+++ utils_assignment_3.py
@@ -21,13 +21,13 @@ class Neuron():
         self.activation_function = hardlim
         self.learning_rate = learning_rate
         if weights is None:
-            self.weights = np.random.normal(loc=0, scale=0.01,
+            self.weights = np.random.normal(loc=0.1, scale=0.01,
                                             size=[self.n_inputs])
         else:
             self.weights = weights
 
         if bias is None:
-            self.bias = np.random.normal(loc=0, scale=0.01)
+            self.bias = np.random.normal(loc=0.1, scale=0.01)
         else:
             self.bias = bias
         self.weight_updates, self.bias_updates = [], []
